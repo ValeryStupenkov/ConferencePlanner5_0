2ab71b022b2d56adb08184706a5530d437b138ba
Valery <stupenkov.val@yandex.ru>
2022-07-04 14:24:59 +0300

    Updated Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,7 @@ namespace ConferencePlanner5._0
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql("Host=localhost;Username=postgres;Password=postgres"));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql("Host=localhost;port=5432;Database=Conferencedb;Username=postgres;Password=12345"));
             services
                 .AddGraphQLServer()
                 .AddQueryType<Query>()
